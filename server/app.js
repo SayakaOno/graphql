fe987b119e5b1445d6109267a24b11833e04220f
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-06-30 16:24:24 -0700

    install mongoose and connect to mongoDB

--- server/app.js
+++ server/app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+const config = require('./config');
 
 const app = express();
+
+mongoose.connect(
+  `mongodb+srv://sayaka:${
+    config.mongoPass
+  }@cluster0-k5u2y.mongodb.net/test?retryWrites=true&w=majority`,
+  { useNewUrlParser: true }
+);
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 
 app.use(
   '/graphql',
