64443ab32dae88949db971bb2ee2f9d34ab251cd
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-07-02 11:30:56 -0700

    add and render BookDetails component

---
+++ client/src/components/BookDetails.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { graphql } from 'react-apollo';
+import { getBookQuery } from '../queries/queries';
+
+const BookDetails = props => {
+  const renderBookDetails = () => {
+    let { book } = props.data;
+    if (book) {
+      return (
+        <div>
+          <h2>{book.name}</h2>
+          <p>{book.genre}</p>
+          <p>{book.author.name}</p>
+          <p>All books by this author</p>
+          <ul className="other-books">
+            {book.author.books.map(book => {
+              return <li key={book.id}>{book.name}</li>;
+            })}
+          </ul>
+        </div>
+      );
+    } else {
+      return <div>No book selected</div>;
+    }
+  };
+  return <div id="book-details">{renderBookDetails()}</div>;
+};
+
+export default graphql(getBookQuery, {
+  options: props => {
+    return {
+      variables: { id: props.bookId }
+    };
+  }
+})(BookDetails);
